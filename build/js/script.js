0f5713c5a81763b5efd650082adba4732a11d0e3
lyndonrey <lyndonreybualat@gmail.com>
2023-07-27 22:27:01 -0600

    update

---
+++ build/js/script.js
@@ -0,0 +1,54 @@
+// Add an event listener to the window for the scroll event
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+
+  // Check the scroll position to determine if the background should be added or removed
+  if (window.scrollY > 0) {
+    navbar.classList.add("text-neutral-900");
+    navbar.classList.add("bg-neutral-100"); // Add the background color
+  } else {
+    navbar.classList.remove("text-neutral-900");
+    navbar.classList.remove("bg-neutral-100"); // Remove the background color
+  }
+});
+// Set the date we're counting down to
+let countDownDate = new Date("Aug 28, 2023 15:37:25").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function () {
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes, and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result to their respective elements
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+// Function to toggle the visibility of the mobile menu
+function toggleMobileMenu() {
+  const navbar = document.getElementById("navbar-default");
+  navbar.classList.toggle("hidden"); // Toggle the "hidden" class to show/hide the menu
+}
+
+// Add an event listener for the phone mode button
+const phoneModeButton = document.querySelector(
+  '[data-collapse-toggle="navbar-default"]'
+);
+phoneModeButton.addEventListener("click", toggleMobileMenu);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./build/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        titan: "'Gloock', serif",
+      },
+    },
   },
   plugins: [],
 };
