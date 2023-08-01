ffb253409a37b5fd20dc04e5e6537e922b9860ce
lyndonrey <lyndonreybualat@gmail.com>
2023-08-01 15:04:46 -0600

    navbar

--- build/js/script.js
+++ build/js/script.js
@@ -4,9 +4,9 @@ window.addEventListener("scroll", function () {
 
   // Check the scroll position to determine if the background should be added or removed
   if (window.scrollY > 0) {
-    navbar.classList.add("md:bg-neutral-900"); // Add the background color
+    navbar.classList.add("bg-neutral-900"); // Add the background color
   } else {
-    navbar.classList.remove("md:bg-neutral-900"); // Remove the background color
+    navbar.classList.remove("bg-neutral-900"); // Remove the background color
   }
 });
 // Set the date we're counting down to
